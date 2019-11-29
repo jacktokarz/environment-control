e188f2de62f57ec07bab722e0fcfd26ef11f5276
fire <fire@fires-MacBook-Pro.local>
2019-11-29 18:05:28 -0500

    coinsound and waterfailure

--- Assets/scripts/CollectibleActivity.cs
+++ Assets/scripts/CollectibleActivity.cs
@@ -3,12 +3,16 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
+
+
 public class CollectibleActivity : MonoBehaviour
 {
-
+    public AudioClip coinsound;
+    private AudioSource source;
     void Start()
     {
-    	Debug.Log("this scene is: "+SceneManager.GetActiveScene().buildIndex);
+        source = GetComponent<AudioSource>();
+        Debug.Log("this scene is: "+SceneManager.GetActiveScene().buildIndex);
     	if( PersistentManager.Instance.Collectibles.Contains(SceneManager.GetActiveScene().buildIndex))
     	{
     		this.gameObject.SetActive(false);
@@ -18,14 +22,16 @@ public class CollectibleActivity : MonoBehaviour
 
     void OnTriggerEnter2D(Collider2D col)
     {
-    	if(col.CompareTag("Player"))
+    	if (col.CompareTag("Player"))
     	{
-    		collect();
-    	}
+            AudioSource.PlayClipAtPoint(coinsound, this.gameObject.transform.position, 0.4f);
+            collect();
+        }
     }
 
     void collect()
     {
+        
 		PersistentManager.Instance.Collectibles.Add(SceneManager.GetActiveScene().buildIndex);
 		this.gameObject.SetActive(false);
     }
