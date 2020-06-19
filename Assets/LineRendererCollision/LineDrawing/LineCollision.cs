0a81361e6261eebf6db82398c9989225032bc210
mikebru <32722658+mikebru@users.noreply.github.com>
2020-06-18 20:00:51 -0500

    Added in line collision sample Scene

---
+++ Assets/LineRendererCollision/LineDrawing/LineCollision.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(EdgeCollider2D))]
+[RequireComponent(typeof(LineRenderer))]
+public class LineCollision : MonoBehaviour
+{
+    private EdgeCollider2D edgeCollider;
+    private LineRenderer lineRenderer;
+
+    public bool Generate;
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        edgeCollider = GetComponent<EdgeCollider2D>();
+        lineRenderer = GetComponent<LineRenderer>();
+
+    }
+
+
+    private void OnDrawGizmosSelected()
+    {
+
+        if (Generate == true)
+        {
+            if (edgeCollider == null)
+            {
+                edgeCollider = GetComponent<EdgeCollider2D>();
+                lineRenderer = GetComponent<LineRenderer>();
+            }
+
+
+            SyncCollision();
+
+           // Generate = false;
+        }
+
+    }
+
+
+    public void SyncCollision()
+    {
+        Vector3[] lineRenderPoints = new Vector3[lineRenderer.positionCount];
+        Vector2[] edgePoints = new Vector2[lineRenderer.positionCount];
+
+        lineRenderer.GetPositions(lineRenderPoints);
+        int i = 0;
+
+
+        foreach (Vector3 position in lineRenderPoints)
+        {
+            edgePoints[i] = new Vector2(position.x, position.y);
+           i+= 1;
+        }
+
+       // edgeCollider.points = new Vector2[lineRenderer.positionCount];
+        edgeCollider.points = edgePoints;
+    }
+
+
+}
