96ce8f0012768cddf21503065f0cbe5af46c5c30
mikebru <32722658+mikebru@users.noreply.github.com>
2020-06-25 17:30:29 -0500

    added in line shrinking functionality

--- Assets/LineRendererCollision/LineDrawing/LineGrow.cs
+++ Assets/LineRendererCollision/LineDrawing/LineGrow.cs
@@ -32,34 +32,42 @@ public class LineGrow : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if(growRate > 0)
+        float currentHeight = (lineRenderer.GetPosition(lineRenderer.positionCount - 1).y);
+
+
+        if (growRate > 0)
         {
-
-            float currentHeight = (lineRenderer.GetPosition(lineRenderer.positionCount - 1).y);
-
             if (currentHeight < MaxHeight)
             {
                 GrowLine();
+            }
+        }else if(growRate < 0)
+        {
+            if(currentHeight > 0)
+            {
+                ShrinkLine();
             }
         }
 
     }
 
-
     void GrowLine()
     {
-
+        //get line position 
         Vector3 currentPosition = lineRenderer.GetPosition(lineRenderer.positionCount - 1);
 
+        //sine wave movement
         Vector3 naturalFlow = (Vector3.right * Mathf.Clamp(Mathf.Sin(currentPosition.y * turbulenceWavelength), -turbulenceStrength, turbulenceStrength)) * Time.deltaTime * growRate;
 
+        //grow direction influence
         Vector3 newPosition = currentPosition + (growRate * growDirection * Time.deltaTime) + naturalFlow;
 
+        //set new line position 
         lineRenderer.SetPosition(lineRenderer.positionCount - 1, newPosition);
 
         float distance = Vector3.Distance(lineRenderer.GetPosition(lineRenderer.positionCount - 2), lineRenderer.GetPosition(lineRenderer.positionCount - 1));
 
-
+        //add a new point
         if(distance > distancePerLine)
         {
             Vector3[] currentPositions = new Vector3[lineRenderer.positionCount];
@@ -85,4 +93,54 @@ public class LineGrow : MonoBehaviour
             lineCollision.SyncCollision();
         }
     }
+
+    void ShrinkLine()
+    {
+
+        //only do this if we have more than one point
+        if (lineRenderer.positionCount > 1)
+        {
+            //get line position 
+            Vector3 currentPosition = lineRenderer.GetPosition(lineRenderer.positionCount - 1);
+
+            Vector3 previousPosition = lineRenderer.GetPosition(lineRenderer.positionCount - 2);
+
+            Vector3 reverseDirection = (currentPosition - previousPosition).normalized;
+
+            //grow direction influence
+            Vector3 newPosition = currentPosition + (growRate * reverseDirection * Time.deltaTime);
+
+            //set new line position 
+            lineRenderer.SetPosition(lineRenderer.positionCount - 1, newPosition);
+
+
+            float distance = Vector3.Distance(lineRenderer.GetPosition(lineRenderer.positionCount - 2), lineRenderer.GetPosition(lineRenderer.positionCount - 1));
+
+            //remove point 
+            if (distance < distancePerLine / 10)
+            {
+                Vector3[] currentPositions = new Vector3[lineRenderer.positionCount];
+
+                Vector3[] newPositions = new Vector3[lineRenderer.positionCount - 1];
+
+                lineRenderer.GetPositions(currentPositions);
+
+                for (int i = 0; i < newPositions.Length; i++)
+                {
+                    newPositions[i] = currentPositions[i];
+                }
+
+                lineRenderer.SetVertexCount(newPositions.Length);
+
+                lineRenderer.SetPositions(newPositions);
+
+                //update colliders 
+                lineCollision.SyncCollision();
+            }
+
+        }
+
+    }
+
+
 }
