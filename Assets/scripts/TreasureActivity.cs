f86c8c8e260b06ead8984e5de045e33e9adfc698
jacktokarz <jacktokarz@gmail.com>
2020-11-16 23:55:06 -0600

    Added more detail to z1-4

--- Assets/scripts/TreasureActivity.cs
+++ Assets/scripts/TreasureActivity.cs
@@ -7,7 +7,7 @@ public class TreasureActivity : MonoBehaviour
 {
     public AudioClip treasureSound;
 	public string treasureId;
-	private Animator openChestAnim;
+	//private Animator openChestAnim;
 	public bool alreadyGotten;
     private AudioSource source;
     public AudioClip labsounds;
@@ -15,11 +15,11 @@ public class TreasureActivity : MonoBehaviour
     void Start()
     {
         source = GetComponent<AudioSource>();
-        openChestAnim = this.GetComponent(typeof (Animator)) as Animator;
+        //openChestAnim = this.GetComponent(typeof (Animator)) as Animator;
     	if( PersistentManager.Instance.TreasureList.Contains(treasureId))
     	{
     		alreadyGotten = true;
-    		openChestAnim.SetBool("opened", true);
+    		//openChestAnim.SetBool("opened", true);
     	}
     }
 
@@ -34,7 +34,7 @@ public class TreasureActivity : MonoBehaviour
     void getTreasure()
     {
         source.PlayOneShot(treasureSound, 2.0f);
-		openChestAnim.SetBool("opening", true);
+		//openChestAnim.SetBool("opening", true);
 		PersistentManager.Instance.TreasureList.Add(treasureId);
 		Queue<string> nextMessages = new Queue<string>();
 		switch (treasureId)
